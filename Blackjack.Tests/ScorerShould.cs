1044796799c6d007f93a98010f57481e96b4d6b8
ES <NoEmail@RightNow.com>
2019-05-25 21:08:10 -0600

    Refactored the ResolvePoints() in Scorer.  Making it clearer as well as facilitating Unit Testing. Added Unit Tests for it as well.

---
+++ Blackjack.Tests/ScorerShould.cs
@@ -0,0 +1,210 @@
+﻿using Blackjack_Mindfire_02;
+using Blackjack_Mindfire_02.Participants;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack.Tests
+{
+	class ScorerShould
+	{
+		Dealer dealer;
+		Player player1;
+		[SetUp]
+		public void Init()
+		{
+			dealer = new Dealer(1);
+			player1 = new Player("Player1");
+		}
+		[TearDown]
+		public void Cleanup()
+		{
+			dealer = null;
+		}
+		[Test]
+		public void PlayerBeatsDealer_ON_FindWinners()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Ace));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer );
+			List<IParticipant> winners = new List<IParticipant>();
+
+			scorer.ResolvePoints();
+			winners = scorer.FindWinners();
+
+			winners.Should().Contain(player1);
+		}
+		[Test]
+		public void DealerBeatsPlayer_ON_FindWinners()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Ace));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+			List<IParticipant> winners = new List<IParticipant>();
+
+			scorer.ResolvePoints();
+			winners = scorer.FindWinners();
+
+			winners.Should().Contain(dealer);
+		}
+		[Test]
+		public void BothPlayerAndDealerWinWhenBoth21AtBeginning_ON_FindWinners()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Ace));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Ace));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+			List<IParticipant> winners = new List<IParticipant>();
+
+			scorer.ResolvePoints();
+			winners = scorer.FindWinners();
+
+			winners.Should().BeEquivalentTo(dealer, player1);
+		}
+		[Test]
+		public void DealerWinsWhenBoth21AfterBeginning_ON_FindWinners()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Eight));
+			dealer.Hand.Add(new Card(Card.Cards.Two));
+			dealer.Hand.Add(new Card(Card.Cards.Ace));
+			player1.Hand.Add(new Card(Card.Cards.Eight));
+			player1.Hand.Add(new Card(Card.Cards.Two));
+			player1.Hand.Add(new Card(Card.Cards.Ace));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+			List<IParticipant> winners = new List<IParticipant>();
+
+			scorer.ResolvePoints();
+			winners = scorer.FindWinners();
+
+			winners.Should().BeEquivalentTo(dealer);
+		}
+		// Individual ResolvePoints() functions
+		[Test]
+		public void OnePointEach_ON_BothDealerAndPlayerHave2CardsAnd21()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Ace));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Ace));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+
+			scorer.BothDealerAndPlayerHave2CardsAnd21();
+
+			dealer.Points.Should().Be(1);
+			player1.Points.Should().Be(1);
+		}
+		[Test]
+		public void TwoPointsEachPlayerDealerBeats_ON_DealerHave2CardsAnd21AndPlayersDont()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Ace));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+
+			scorer.DealerHave2CardsAnd21AndPlayersDont();
+
+			dealer.Points.Should().Be(2);
+			player1.Points.Should().Be(0);
+		}
+		[Test]
+		public void PlayerTwoPointsWhenBeatDealerWith2CardsAnd21Points_ON_PlayerHave2CardsAnd21AndDealerDoesnt()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Ace));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+
+			scorer.PlayerHave2CardsAnd21AndDealerDoesnt();
+
+			dealer.Points.Should().Be(0);
+			player1.Points.Should().Be(2);
+		}
+		[Test]
+		public void OnePointForDealerWhenWin_BY_BothPlayerDealerSameValueAndNotAt2Cards()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Five));
+			dealer.Hand.Add(new Card(Card.Cards.Five));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Five));
+			player1.Hand.Add(new Card(Card.Cards.Five));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+
+			scorer.BothPlayerDealerSameValueAndNotAt2CardsAnd21();
+
+			dealer.Points.Should().Be(1);
+			player1.Points.Should().Be(0);
+		}
+		[Test]
+		public void DealerGetsOnePointPerPlayer_21With3Cards_BY_DealerMorePointsThanPlayerEitherTwoCardsOrLessThan21()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Six));
+			dealer.Hand.Add(new Card(Card.Cards.Five));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Five));
+			player1.Hand.Add(new Card(Card.Cards.Five));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+
+			scorer.DealerMorePointsThanPlayerEitherTwoCardsOrLessThan21();
+
+			dealer.Points.Should().Be(1);
+			player1.Points.Should().Be(0);
+		}
+		[Test]
+		public void DealerGetsOnePointPerPlayer_20With2Cards_BY_DealerMorePointsThanPlayerEitherTwoCardsOrLessThan21()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Four));
+			player1.Hand.Add(new Card(Card.Cards.Five));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+
+			scorer.DealerMorePointsThanPlayerEitherTwoCardsOrLessThan21();
+
+			dealer.Points.Should().Be(1);
+			player1.Points.Should().Be(0);
+		}
+		[Test]
+		public void PlayerGetsOnePoint_21With3Cards_BY_PlayerMorePointsThanDealerEitherTwoCardsOrLessThan21()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Five));
+			dealer.Hand.Add(new Card(Card.Cards.Five));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Five));
+			player1.Hand.Add(new Card(Card.Cards.Six));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+
+			scorer.PlayerMorePointsThanDealerEitherTwoCardsOrLessThan21();
+
+			dealer.Points.Should().Be(0);
+			player1.Points.Should().Be(1);
+		}
+		[Test]
+		public void PlayerGetsOnePoint_20With2Cards_BY_PlayerMorePointsThanDealerEitherTwoCardsOrLessThan21()
+		{
+			dealer.Hand.Add(new Card(Card.Cards.Ten));
+			dealer.Hand.Add(new Card(Card.Cards.Four));
+			dealer.Hand.Add(new Card(Card.Cards.Five));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			player1.Hand.Add(new Card(Card.Cards.Ten));
+			Scorer scorer = new Scorer(new List<Player>() { player1 }, dealer);
+
+			scorer.PlayerMorePointsThanDealerEitherTwoCardsOrLessThan21();
+
+			dealer.Points.Should().Be(0);
+			player1.Points.Should().Be(1);
+		}
+	}
+}
--- Blackjack_Mindfire_02/Scorer.cs
+++ Blackjack_Mindfire_02/Scorer.cs
@@ -7,98 +7,119 @@ using Blackjack_Mindfire_02.Participants;
 
 namespace Blackjack_Mindfire_02
 {
-    class Scorer
-    {
-        List<Player> players;
-        Dealer dealer;
+	public class Scorer
+	{
+		List<Player> players;
+		Dealer dealer;
 
-        public Scorer(List<Player> players, Dealer dealer)
-        {
-            this.players = players;
-            this.dealer = dealer;
-        }
-
-        public void ResolvePoints()
-        {
-            // if both dealer and the player have 2 cards and 21 then each get 1 point
-            if (dealer.Hand.Count == 2 && dealer.Hand.HighestHand() == 21 &&
-                players.Where(p => p.Hand.HighestHand() == 21).Where(p => p.Hand.Count == 2).Any())
-            {
-                List<Player> playersTwo21 = players.Where(p => p.Hand.HighestHand() == 21).Where(p => p.Hand.Count == 2).ToList<Player>();
-                foreach (Player player in playersTwo21)
-                {
-                    dealer.Points++;
-                    player.Points++;
-                }
-            }
-            // If dealer has 2 cards and 21, and players do not have 2 and 21, then give him 2 points for each player he beats
-            if (dealer.Hand.Count == 2 && dealer.Hand.HighestHand() == 21 &&
-                players.Where(p => p.Hand.HighestHand() != 21 || p.Hand.Count > 2).Any())
-            {
-                List<Player> playersLess21 = players.Where(p => p.Hand.HighestHand() != 21 || p.Hand.Count > 2).ToList<Player>();
-                foreach (Player player in playersLess21)
-                {
-                    dealer.Points += 2;
-                }
-            }
-            // If player has 2 cards and 21, and dealer does not, then give the player 2 points
-            if ((dealer.Hand.HighestHand() != 21 || dealer.Hand.Count > 2) &&
-                players.Where(p => p.Hand.HighestHand() == 21 && p.Hand.Count == 2).Any())
-            {
-                List<Player> players21DealerLose = players.Where(p => p.Hand.HighestHand() == 21 && p.Hand.Count == 2).ToList<Player>();
-                foreach (Player player in players21DealerLose)
-                {
-                    player.Points += 2;
-                }
-            }
-            // If the dealer and the player have the same value (and they are not both at 2 cards and 21), then the dealer gets 1 point
-            if (!dealer.Hand.Bust && players.Where(p => p.Hand.HighestHand() == dealer.Hand.HighestHand() && (dealer.Hand.HighestHand() < 21) || dealer.Hand.Count > 2).Any())
-            {
-                List<Player> playersEqual = players.Where(p => p.Hand.HighestHand() == dealer.Hand.HighestHand() && !p.Hand.Bust && (p.Hand.Count > 2 || p.Hand.HighestHand() < 21)).ToList<Player>();
-                foreach (Player player in playersEqual)
-                {
-                    dealer.Points++;
-                }
-            }
-
-            // If the dealer has more points than another player (without busting) and has either more than 2 cards or less than 21 then he gets 1 point for each person he beats
-            if (!dealer.Hand.Bust && players.Where(p => p.Hand.HighestHand() < dealer.Hand.HighestHand() && (dealer.Hand.HighestHand() < 21 || dealer.Hand.Count > 2)).Any())
-            {
-                List<Player> playersLost = players.Where(p => p.Hand.HighestHand() < dealer.Hand.HighestHand() && (dealer.Hand.HighestHand() < 21 || dealer.Hand.Count > 2)).ToList<Player>();
-                foreach (Player player in playersLost)
-                {
-                    dealer.Points++;
-                }
-            }
-
-            // If the player has more points than the dealer (without busting) and has either more than 2 cards or less than 21 then the player gets 1 point
-            if (players.Where(p => p.Hand.HighestHand() > dealer.Hand.HighestHand() && (p.Hand.HighestHand() < 21 || p.Hand.Count > 2) && p.Hand.Bust != true).Any())
-            {
-                List<Player> playersWon = players.Where(p => p.Hand.HighestHand() > dealer.Hand.HighestHand() && (p.Hand.HighestHand() < 21 || p.Hand.Count > 2) && p.Hand.Bust != true).ToList<Player>();
-                foreach (Player player in playersWon)
-                {
-                    player.Points++;
-                }
-            }
-        }
-        public List<IParticipant> FindWinners()
-        {
-            List<IParticipant> winners = new List<IParticipant>();
-            if (!dealer.Hand.Bust)
-                winners.Add(dealer);
-            foreach (Player player in players.Where(p => p.Hand.Bust != true))
-            {
-                if (winners.Count == 0)
-                    winners.Add(player);
-                if (winners[0].Points < player.Points)
-                {
-                    winners.Clear();
-                    winners.Add(player);
-                }
-                if (player.Points == winners[0].Points && player != winners[0])
-                    winners.Add(player);
-            }
-            return winners;
-        }
-    }
+		public Scorer(List<Player> players, Dealer dealer)
+		{
+			this.players = players;
+			this.dealer = dealer;
+		}
+		public void ResolvePoints()
+		{
+			BothDealerAndPlayerHave2CardsAnd21();
+			DealerHave2CardsAnd21AndPlayersDont();
+			PlayerHave2CardsAnd21AndDealerDoesnt();
+			BothPlayerDealerSameValueAndNotAt2CardsAnd21();
+			DealerMorePointsThanPlayerEitherTwoCardsOrLessThan21();
+			PlayerMorePointsThanDealerEitherTwoCardsOrLessThan21();
+		}
+		public void BothDealerAndPlayerHave2CardsAnd21()
+		{
+			// if both dealer and the player have 2 cards and 21 then each get 1 point
+			if (dealer.Hand.Count == 2 && dealer.Hand.HighestHand() == 21 &&
+				players.Where(p => p.Hand.HighestHand() == 21).Where(p => p.Hand.Count == 2).Any())
+			{
+				List<Player> playersTwo21 = players.Where(p => p.Hand.HighestHand() == 21).Where(p => p.Hand.Count == 2).ToList<Player>();
+				foreach (Player player in playersTwo21)
+				{
+					dealer.Points++;
+					player.Points++;
+				}
+			}
+		}
+		public void DealerHave2CardsAnd21AndPlayersDont()
+		{
+			// If dealer has 2 cards and 21, and players do not have 2 and 21, then give him 2 points for each player he beats
+			if (dealer.Hand.Count == 2 && dealer.Hand.HighestHand() == 21 &&
+				players.Where(p => p.Hand.HighestHand() != 21 || p.Hand.Count > 2).Any())
+			{
+				List<Player> playersLess21 = players.Where(p => p.Hand.HighestHand() != 21 || p.Hand.Count > 2).ToList<Player>();
+				foreach (Player player in playersLess21)
+				{
+					dealer.Points += 2;
+				}
+			}
+		}
+		public void PlayerHave2CardsAnd21AndDealerDoesnt()
+		{
+			// If player has 2 cards and 21, and dealer does not, then give the player 2 points
+			if ((dealer.Hand.HighestHand() != 21 || dealer.Hand.Count > 2) &&
+				players.Where(p => p.Hand.HighestHand() == 21 && p.Hand.Count == 2).Any())
+			{
+				List<Player> players21DealerLose = players.Where(p => p.Hand.HighestHand() == 21 && p.Hand.Count == 2).ToList<Player>();
+				foreach (Player player in players21DealerLose)
+				{
+					player.Points += 2;
+				}
+			}
+		}
+		public void BothPlayerDealerSameValueAndNotAt2CardsAnd21()
+		{
+			// If the dealer and the player have the same value (and they are not both at 2 cards and 21), then the dealer gets 1 point
+			if (!dealer.Hand.Bust && players.Where(p => p.Hand.HighestHand() == dealer.Hand.HighestHand() && (dealer.Hand.HighestHand() < 21) || dealer.Hand.Count > 2).Any())
+			{
+				List<Player> playersEqual = players.Where(p => p.Hand.HighestHand() == dealer.Hand.HighestHand() && !p.Hand.Bust && (p.Hand.Count > 2 || p.Hand.HighestHand() < 21)).ToList<Player>();
+				foreach (Player player in playersEqual)
+				{
+					dealer.Points++;
+				}
+			}
+		}
+		public void DealerMorePointsThanPlayerEitherTwoCardsOrLessThan21()
+		{
+			// If the dealer has more points than another player (without busting) and has either more than 2 cards or less than 21 then he gets 1 point for each person he beats
+			if (!dealer.Hand.Bust && players.Where(p => p.Hand.HighestHand() < dealer.Hand.HighestHand() && (dealer.Hand.HighestHand() < 21 || dealer.Hand.Count > 2)).Any())
+			{
+				List<Player> playersLost = players.Where(p => p.Hand.HighestHand() < dealer.Hand.HighestHand() && (dealer.Hand.HighestHand() < 21 || dealer.Hand.Count > 2)).ToList<Player>();
+				foreach (Player player in playersLost)
+				{
+					dealer.Points++;
+				}
+			}
+		}
+		public void PlayerMorePointsThanDealerEitherTwoCardsOrLessThan21()
+		{
+			// If the player has more points than the dealer (without busting) and has either more than 2 cards or less than 21 then the player gets 1 point
+			if (players.Where(p => p.Hand.HighestHand() > dealer.Hand.HighestHand() && (p.Hand.HighestHand() < 21 || p.Hand.Count > 2) && p.Hand.Bust != true).Any())
+			{
+				List<Player> playersWon = players.Where(p => p.Hand.HighestHand() > dealer.Hand.HighestHand() && (p.Hand.HighestHand() < 21 || p.Hand.Count > 2) && p.Hand.Bust != true).ToList<Player>();
+				foreach (Player player in playersWon)
+				{
+					player.Points++;
+				}
+			}
+		}
+		public List<IParticipant> FindWinners()
+		{
+			List<IParticipant> winners = new List<IParticipant>();
+			if (!dealer.Hand.Bust)
+				winners.Add(dealer);
+			foreach (Player player in players.Where(p => p.Hand.Bust != true))
+			{
+				if (winners.Count == 0)
+					winners.Add(player);
+				if (winners[0].Points < player.Points)
+				{
+					winners.Clear();
+					winners.Add(player);
+				}
+				if (player.Points == winners[0].Points && player != winners[0])
+					winners.Add(player);
+			}
+			return winners;
+		}
+	}
 }
